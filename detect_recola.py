72ac4ebf08dd799f4f9f6f2c38ecc869831e43a8
xiacan <can.xia@horizon.ai>
2019-03-10 11:11:19 +0800

    add detect func

--- detect_recola.py
+++ detect_recola.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 from pathlib import Path
 import mxnet as mx
 from mtcnn_detector import MtcnnDetector
@@ -9,29 +11,20 @@ from scipy.misc import imresize
 import cv2
 from skimage import color
 from tqdm import tqdm
+import os.path as osp
 
-if __name__ == '__main__':
-    with open('config/path.json', 'r') as fp:
-        config = json.load(fp)
-    root_dir = Path(config['windows'])
-    video_dir = root_dir / 'RECOLA-Video-recordings'
-    anno_dir = root_dir / 'RECOLA-Annotation/emotional_behaviour'
-    outdir = root_dir / 'recola_out'
 
+def track_video_face(video_dir, subject_id, outdir):
     detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(0), num_worker=4, accurate_landmark=False)
-    subject_id = 16
     video_path = video_dir / 'P{}.mp4'.format(subject_id)
     clip = VideoFileClip(str(video_path))
     clip = resize(clip, (160, 90))
     face_frames = []
     for i, frame in tqdm(enumerate(clip.iter_frames())):
-        # frame = color.rgb2gray(frame)
         results = detector.detect_face(frame)
         if results is not None:
             total_boxes = results[0]
             for box in total_boxes:
-                # draw = frame.copy()
-                # cv2.rectangle(draw, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,255,255))
                 if (box[2] - box[0]) > 24 and (box[3] - box[1]) > 24:
                     tmp_x1, tmp_y1, tmp_x2, tmp_y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
                     tmp_x1 = max(tmp_x1, 0)
@@ -45,7 +38,8 @@ if __name__ == '__main__':
                     else:
                         tmp_face_gray = color.rgb2gray(tmp_face)
                         last_face_gray = color.rgb2gray(last_face)
-                        sml = ssim(last_face_gray, tmp_face_gray, data_range=(tmp_face_gray.max() - tmp_face_gray.min()))
+                        sml = ssim(last_face_gray, tmp_face_gray,
+                                   data_range=(tmp_face_gray.max() - tmp_face_gray.min()))
                         if sml > 0.3:
                             x1, y1, x2, y2 = tmp_x1, tmp_y1, tmp_x2, tmp_y2
                             face = tmp_face
@@ -55,9 +49,20 @@ if __name__ == '__main__':
                     face = imresize(frame[y1:y2, x1:x2], (48, 48))
         else:
             face = imresize(frame[y1:y2, x1:x2], (48, 48))
-        # cv2.imshow('face', face)
-        # cv2.waitKey(1)
         face_frames.append(face)
         last_face = face
     res_clip = ImageSequenceClip(face_frames, fps=25)
-    res_clip.to_videofile('16.mp4')
+    face_videodir = str(outdir / 'Video')
+    if not osp.isdir(face_videodir):
+        os.makedirs(face_videodir)
+    res_clip.to_videofile('{}/P{}.mp4'.format(face_videodir, subject_id))
+
+
+if __name__ == '__main__':
+    with open('config/path.json', 'r') as fp:
+        config = json.load(fp)
+    root_dir = Path(config['windows'])
+    video_dir = root_dir / 'RECOLA-Video-recordings'
+    anno_dir = root_dir / 'RECOLA-Annotation/emotional_behaviour'
+    outdir = root_dir / 'recola_out'
+    track_video_face(video_dir, 16, outdir)
