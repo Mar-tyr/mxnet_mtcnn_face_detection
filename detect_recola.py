f5ed432687793e666f66a4375af2b0f2d6046be6
can.xia <can.xia@horizon.ai>
2019-03-10 14:52:07 +0800

    detect all video

--- detect_recola.py
+++ detect_recola.py
@@ -15,42 +15,37 @@ import os.path as osp
 
 
 def track_video_face(video_dir, subject_id, outdir):
-    detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(0), num_worker=4, accurate_landmark=False)
+    detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(0), num_worker=8, accurate_landmark=False)
     video_path = video_dir / 'P{}.mp4'.format(subject_id)
     clip = VideoFileClip(str(video_path))
     clip = resize(clip, (160, 90))
     face_frames = []
-    for i, frame in tqdm(enumerate(clip.iter_frames())):
-        results = detector.detect_face(frame)
-        if results is not None:
-            total_boxes = results[0]
-            for box in total_boxes:
-                if (box[2] - box[0]) > 24 and (box[3] - box[1]) > 24:
-                    tmp_x1, tmp_y1, tmp_x2, tmp_y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
-                    tmp_x1 = max(tmp_x1, 0)
-                    tmp_y1 = max(tmp_y1, 0)
-                    tmp_x2 = max(tmp_x2, 0)
-                    tmp_y2 = max(tmp_y2, 0)
-                    tmp_face = imresize(frame[tmp_y1:tmp_y2, tmp_x1:tmp_x2], (48, 48))
-                    if i == 0:
+    first_undetected_n = 0
+    for i, frame in tqdm(enumerate(clip.iter_frames()), desc=str(subject_id)):
+        try:
+            results = detector.detect_face(frame)
+            if results is not None:
+                total_boxes = results[0]
+                for box in total_boxes:
+                    if (box[2] - box[0]) > 24 and (box[3] - box[1]) > 24:
+                        tmp_x1, tmp_y1, tmp_x2, tmp_y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+                        tmp_x1 = max(tmp_x1, 0)
+                        tmp_y1 = max(tmp_y1, 0)
+                        tmp_x2 = max(tmp_x2, 0)
+                        tmp_y2 = max(tmp_y2, 0)
+                        tmp_face = imresize(frame[tmp_y1:tmp_y2, tmp_x1:tmp_x2], (48, 48))
                         x1, y1, x2, y2 = tmp_x1, tmp_y1, tmp_x2, tmp_y2
                         face = tmp_face
                     else:
-                        tmp_face_gray = color.rgb2gray(tmp_face)
-                        last_face_gray = color.rgb2gray(last_face)
-                        sml = ssim(last_face_gray, tmp_face_gray,
-                                   data_range=(tmp_face_gray.max() - tmp_face_gray.min()))
-                        if sml > 0.3:
-                            x1, y1, x2, y2 = tmp_x1, tmp_y1, tmp_x2, tmp_y2
-                            face = tmp_face
-                        else:
-                            face = imresize(frame[y1:y2, x1:x2], (48, 48))
-                else:
-                    face = imresize(frame[y1:y2, x1:x2], (48, 48))
-        else:
-            face = imresize(frame[y1:y2, x1:x2], (48, 48))
-        face_frames.append(face)
-        last_face = face
+                        face = imresize(frame[y1:y2, x1:x2], (48, 48))
+            else:
+                face = imresize(frame[y1:y2, x1:x2], (48, 48))
+            face_frames.append(face)
+        except UnboundLocalError:
+            first_undetected_n += 1
+    print('Need to fill to beginning:', first_undetected_n)
+    for i in range(first_undetected_n):
+        face_frames.insert(0, face_frames[0])
     res_clip = ImageSequenceClip(face_frames, fps=25)
     face_videodir = str(outdir / 'Video')
     if not osp.isdir(face_videodir):
@@ -61,8 +56,10 @@ def track_video_face(video_dir, subject_id, outdir):
 if __name__ == '__main__':
     with open('config/path.json', 'r') as fp:
         config = json.load(fp)
-    root_dir = Path(config['windows'])
+    root_dir = Path(config['gpu_025'])
     video_dir = root_dir / 'RECOLA-Video-recordings'
     anno_dir = root_dir / 'RECOLA-Annotation/emotional_behaviour'
     outdir = root_dir / 'recola_out'
-    track_video_face(video_dir, 16, outdir)
+    subject_ids = config['ids']
+    for subject_id in subject_ids:
+        track_video_face(video_dir, subject_id, outdir)
